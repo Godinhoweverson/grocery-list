0daa0fe8194a4e00866666efcddc5811bd67c5b1
Weverson Godinho <weverson.godinho@icloud.com>
2025-11-28 20:12:30 +0000

    conclude the application

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ let ulList = document.getElementById("list");
 
 
 AddgroceryBtn.addEventListener('click', function(e){
-   let product = Addgrocery.value;
-   addProduct(product)
+   let product = Addgrocery.value.trim();
+   if(product === "") return;
+   productList(product)
+   Addgrocery.value = '';
 });
 
-function addProduct(product){
-    let li = document.createElement("li");
-    li.textContent = product;
-    ulList.appendChild(li);
+function productList(product) {
+  let li = document.createElement("li");
+  li.textContent = product;
+  let span = document.createElement("span");
+  span.className = "close";
+  span.textContent = "delete";
+  span.addEventListener("click", function (e) {
+    const productDeleted = e.target.closest("li");
+    productDeleted.remove();
+  });
+  li.appendChild(span);
+  ulList.appendChild(li);
 }
+
